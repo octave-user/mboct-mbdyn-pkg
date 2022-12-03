d5835c1fbb403be5d362ab49a3dce59fb0c7ff2c
Reinard <octave-user@a1.net>
2022-12-03 19:12:07 +0100

    Add output of stress stiffening matrices

--- inst/mbdyn_pre_write_fem_data.m
+++ inst/mbdyn_pre_write_fem_data.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2014(-2020) Reinhard <octave-user@a1.net>
+## Copyright (C) 2014(-2022) Reinhard <octave-user@a1.net>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 ## along with this program; If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*-
-## @deftypefn {Function File} mbdyn_pre_write_fem_data(@var{fem_filename}, @var{Mred}, @var{Dred}, @var{Sred}, @var{Tred}, @var{X0}, @var{ured0}, @var{dured_dt0}, @var{diagM}, @var{m}, @var{Xgc}, @var{Jgc}, @var{node_list}, @var{Inv3}, @var{Inv4}, @var{Inv5}, @var{Inv8}, @var{Inv9})
+## @deftypefn {Function File} mbdyn_pre_write_fem_data(@var{fem_filename}, @var{Mred}, @var{Dred}, @var{Kred}, @var{Tred}, @var{X0}, @var{ured0}, @var{dured_dt0}, @var{diagM}, @var{m}, @var{Xgc}, @var{Jgc}, @var{node_list}, @var{Inv3}, @var{Inv4}, @var{Inv5}, @var{Inv8}, @var{Inv9}, @var{index_KTAU0red}, @var{KTAU0red})
 ##
 ## Creates a FEM input file for MBDyn's model element.
 ##
@@ -22,7 +22,7 @@
 ##
 ## @var{Mred} @dots{} reduced order mass matrix
 ##
-## @var{Sred} @dots{} reduced order stiffness matrix
+## @var{Kred} @dots{} reduced order stiffness matrix
 ##
 ## @var{Tred} @dots{} matrix of mode shapes at exposed nodes
 ##
@@ -52,6 +52,10 @@
 ##
 ## @var{Inv9} @dots{} Quadratic coupling terms
 ##
+## @var{index_KTAU0red} @dots{} index vector for stress stiffening matrix
+##
+## @var{KTAU0red} @dots{} Stress stiffening matrix
+##
 ## References:
 ## @nospell{MBDyn Theory and Developer's Manual Version 1.7.1}
 ## @nospell{Pierangelo Masarati}
@@ -60,8 +64,29 @@
 ## @nospell{January 9, 2017}
 ## @end deftypefn
 
-function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured0, dured_dt0, diagM, m, Xgc, Jgc, node_list, Inv3, Inv4, Inv5, Inv8, Inv9)
-  if (nargin < 5)
+## 1 fem_filename
+## 2 Mred
+## 3 Dred
+## 4 Kred
+## 5 Tred
+## 6 X0
+## 7 ured0
+## 8 dured_dt0
+## 9 diagM
+## 10 m
+## 11 Xgc
+## 12 Jgc
+## 13 node_list
+## 14 Inv3
+## 15 Inv4
+## 16 Inv5
+## 17 Inv8
+## 18 Inv9
+## 19 index_KTAU0red
+## 20 KTAU0red
+
+function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Kred, Tred, X0, ured0, dured_dt0, diagM, m, Xgc, Jgc, node_list, Inv3, Inv4, Inv5, Inv8, Inv9, index_KTAU0red, KTAU0red)
+  if (nargin < 6 || nargin > 20)
     print_usage();
   endif
 
@@ -75,11 +100,11 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
 
   N = columns(Mred);
 
-  if (~ismatrix(Sred) || ~isreal(Sred) || rows(Sred) ~= N || columns(Sred) ~= N)
-    error("Sred must be a square real matrix and must have the same dimensions like Mred");
+  if (~ismatrix(Kred) || ~isreal(Kred) || rows(Kred) ~= N || columns(Kred) ~= N)
+    error("Kred must be a square real matrix and must have the same dimensions like Mred");
   endif
 
-  if (~ismatrix(Dred) || ~isreal(Dred) || numel(Dred) > 0 && (rows(Dred) ~= N || columns(Dred) ~= N))
+  if (~ismatrix(Dred) || ~isreal(Dred) || ~isempty(Dred) && (rows(Dred) ~= N || columns(Dred) ~= N))
     error("Dred must be a square real matrix and must have the same dimensions like Mred");
   endif
 
@@ -91,56 +116,66 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
     error("X0 must be a real 3 x rows(Tred) / 6 matrix");
   endif
 
-  if (nargin >= 6)
+  if (nargin >= 7)
     if (~isvector(ured0) || ~isreal(ured0) || columns(ured0) ~= 1 || rows(ured0) ~= N)
       error("ured0 must be real vector with the same number of rows like Mred");
     endif
   endif
 
-  if (nargin >= 7)
+  if (nargin >= 8)
     if (~isvector(dured_dt0) || ~isreal(dured_dt0) || columns(dured_dt0) ~= 1 || rows(dured_dt0) ~= N)
       error("dured_dt0 must be real vector with the same number of rows like Mred");
     endif
   endif
 
-  if (nargin >= 8 && numel(diagM) ~= 0)
+  if (nargin >= 9 && ~isempty(diagM))
     if (~isvector(diagM) || ~isreal(diagM) || numel(diagM) ~= columns(X0) * 6)
       error("diagM must be a real vector with the same length as the number of columns of X0");
     endif
   endif
 
-  if (nargin >= 14 && numel(Inv3) ~= 0)
+  if (nargin >= 14 && ~isempty(Inv3))
     if (~ismatrix(Inv3) || ~isreal(Inv3) || rows(Inv3) ~= 3 || columns(Inv3) ~= N)
       error("Inv3 must be a real 3 x N matrix");
     endif
   endif
 
-  if (nargin >= 15 && numel(Inv4) ~= 0)
+  if (nargin >= 15 && ~isempty(Inv4))
     if (~ismatrix(Inv4) || ~isreal(Inv4) || rows(Inv4) ~= 3 || columns(Inv4) ~= N)
       error("Inv4 must be a real 3 x N matrix");
     endif
   endif
 
-  if (nargin >= 16 && numel(Inv5) ~= 0)
+  if (nargin >= 16 && ~isempty(Inv5))
     if (~isreal(Inv5) || rows(Inv5) ~= 3 || columns(Inv5) ~= N || size(Inv5, 3) ~= N)
       error("Inv5 must be a real 3 x N x N matrix");
     endif
   endif
 
-  if (nargin >= 17 && numel(Inv8) ~= 0)
+  if (nargin >= 17 && ~isempty(Inv8))
     if (~isreal(Inv8) || rows(Inv8) ~= 3 || columns(Inv8) ~= 3 || size(Inv8, 3) ~= N)
       error("Inv8 must be a real 3 x 3 x N matrix");
     endif
   endif
 
-  if (nargin >= 18 && numel(Inv9) ~= 0)
+  if (nargin >= 18 && ~isempty(Inv9))
     if (~isreal(Inv9) || rows(Inv9) ~= 3 || columns(Inv9) ~= 3 || size(Inv9, 3) ~= N  || size(Inv9, 4) ~= N)
       error("Inv9 must be a real 3 x 3 x N x N matrix");
     endif
   endif
 
+  if (nargin >= 19 && ~isempty(KTAU0red))
+    if (~(isvector(index_KTAU0red) && numel(index_KTAU0red) == size(KTAU0red, 3)))
+      error("size of KTAU0red and index_KTAU0red does not match");
+    endif
+
+    if (~(isreal(KTAU0red) && rows(KTAU0red) == N && columns(KTAU0red) == N))
+      error("KTAU0red must be a real N x N x M matrix");
+    endif
+  endif
+
   fd = -1;
-  
+
   unwind_protect
     [fd, msg] = fopen(fem_filename, "wt");
 
@@ -155,7 +190,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
     CONSTRAINT = 0;
     REJECTED = 0;
 
-    if (nargin < 13 || ~numel(node_list))
+    if (nargin < 13 || isempty(node_list))
       node_list = 1:NODE;
     endif
 
@@ -171,14 +206,14 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
     fprintf(fd, "%d ", node_list);
     fprintf(fd, "\n**\n");
 
-    if (nargin >= 6)
+    if (nargin >= 7 && ~isempty(ured0))
       fprintf(fd, "\n** RECORD GROUP 3, INITIAL MODAL DISPLACEMENTS\n");
       fprintf(fd, "%.16e ", ured0);
       fprintf(fd, "\n**\n");
     endif
 
-    if (nargin >= 7)
-      fprintf(fd, "\n** RECORD GROUP 4, INITIAL MODALVELOCITIES\n");
+    if (nargin >= 8 && ~isempty(dured_dt0))
+      fprintf(fd, "\n** RECORD GROUP 4, INITIAL MODAL VELOCITIES\n");
       fprintf(fd, "%.16e ", dured_dt0);
       fprintf(fd, "\n**\n");
     endif
@@ -207,20 +242,20 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
     fprintf(fd, "**\n");
     fprintf(fd, "** RECORD GROUP 10, MODAL STIFFNESS MATRIX\n");
 
-    for i=1:rows(Sred)
-      fprintf(fd, "%.16e ", Sred(i, :));
+    for i=1:rows(Kred)
+      fprintf(fd, "%.16e ", Kred(i, :));
       fprintf(fd, "\n");
     endfor
 
     fprintf(fd, "**\n");
 
-    if (nargin >= 8 && numel(diagM) > 0)
+    if (nargin >= 9 && ~isempty(diagM))
       fprintf(fd, "** RECORD GROUP 11, DIAGONAL OF LUMPED MASS MATRIX\n");
       fprintf(fd, "%.16e %.16e %.16e %.16e %.16e %.16e\n", diagM);
       fprintf(fd, "**\n");
     endif
 
-    if (nargin >= 11 && numel(Jgc) > 0)
+    if (nargin >= 12 && ~isempty(Jgc))
       fprintf(fd, "** RECORD GROUP 12, RIGID BODY INERTIA MATRIX\n");
       fprintf(fd, "%.16e\n", m);
       fprintf(fd, "%.16e %.16e %.16e\n", Xgc);
@@ -228,7 +263,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
       fprintf(fd, "**\n");
     endif
 
-    if (numel(Dred) > 0)
+    if (~isempty(Dred))
       fprintf(fd, "**\n");
       fprintf(fd, "** RECORD GROUP 13, MODAL DAMPING MATRIX\n");
 
@@ -240,7 +275,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
       fprintf(fd, "**\n");
     endif
 
-    if (nargin >= 14 && numel(Inv3) > 0)
+    if (nargin >= 14 && ~isempty(Inv3))
       fprintf(fd, "**\n");
       fprintf(fd, "** RECORD GROUP 14, INVARIANT 3\n");
 
@@ -252,7 +287,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
       fprintf(fd, "**\n");
     endif
 
-    if (nargin >= 15 && numel(Inv4) > 0)
+    if (nargin >= 15 && ~isempty(Inv4))
       fprintf(fd, "**\n");
       fprintf(fd, "** RECORD GROUP 15, INVARIANT 4\n");
 
@@ -264,7 +299,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
       fprintf(fd,"**\n");
     endif
 
-    if (nargin >= 17 && numel(Inv8) > 0)
+    if (nargin >= 17 && ~isempty(Inv8))
       fprintf(fd, "**\n");
       fprintf(fd, "** RECORD GROUP 16, INVARIANT 8\n");
 
@@ -278,7 +313,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
       fprintf(fd, "**\n");
     endif
 
-    if (nargin >= 16 && numel(Inv5) > 0)
+    if (nargin >= 16 && ~isempty(Inv5))
       fprintf(fd, "**\n");
       fprintf(fd, "** RECORD GROUP 17, INVARIANT 5\n");
 
@@ -292,7 +327,7 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
       fprintf(fd, "**\n");
     endif
 
-    if (nargin >= 16 && numel(Inv9) > 0)
+    if (nargin >= 18 && ~isempty(Inv9))
       fprintf(fd, "**\n");
       fprintf(fd, "** RECORD GROUP 18, INVARIANT 9\n");
 
@@ -303,6 +338,22 @@ function mbdyn_pre_write_fem_data(fem_filename, Mred, Dred, Sred, Tred, X0, ured
           endfor
         endfor
         fprintf(fd, "\n");
+      endfor
+
+      fprintf(fd, "**\n");
+    endif
+
+    if (nargin >= 19 && ~isempty(KTAU0red))
+      fprintf(fd, "**\n");
+      fprintf(fd, "** RECORD GROUP 19, MODAL STRESS STIFFENING MATRIX\n");
+      fprintf(fd, "%d\n", size(KTAU0red, 3));
+
+      for j=1:size(KTAU0red, 3)
+        fprintf(fd, "%d\n", index_KTAU0red(j));
+        for i=1:rows(KTAU0red)
+          fprintf(fd, "%.16e ", KTAU0red(i, :, j));
+          fprintf(fd, "\n");
+        endfor
       endfor
 
       fprintf(fd, "**\n");

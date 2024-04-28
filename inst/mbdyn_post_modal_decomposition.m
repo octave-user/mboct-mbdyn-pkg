1c57f478a6df316d4eb7a545f0ea27684baeeab8
Reinard <octave-user@a1.net>
2024-04-28 22:01:09 +0200

    Test bi-modal decomposition

--- inst/mbdyn_post_modal_decomposition.m
+++ inst/mbdyn_post_modal_decomposition.m
@@ -5,6 +5,10 @@ function [r, l, lambda, err, LAMBDA] = mbdyn_post_modal_decomposition(A, B, dCoe
     error("incompatible matrix size of A and B");
   endif
 
+  if (~(isreal(A) && isreal(B)))
+    error("A and B must be real matrices");
+  endif
+  
   pkg load mboct-fem-pkg;
 
   Bfact = fem_sol_factor(B, opts);
@@ -13,13 +17,16 @@ function [r, l, lambda, err, LAMBDA] = mbdyn_post_modal_decomposition(A, B, dCoe
 
   opts.issym = false;
   opts.isreal = true;
-  opts.p = min(2 * k, n);
+
+  if (~isfield(opts, "p"))
+    opts.p = min(4 * k, n);
+  endif
 
   rndstate = rand("state");
 
   unwind_protect
     rand("seed", 0);
-    [V, LAMBDA, flag] = eigs(oper_func, 2 * n, k, sigma, opts);
+    [V, LAMBDA, flag] = eigs(oper_func, 2 * n, 2 * k, sigma, opts);
   unwind_protect_cleanup
     rand("state", rndstate);
   end_unwind_protect
@@ -27,11 +34,11 @@ function [r, l, lambda, err, LAMBDA] = mbdyn_post_modal_decomposition(A, B, dCoe
   if (flag ~= 0)
     error("eigs failed with status %d", flag);
   endif
-
-  LAMBDA = diag(LAMBDA)(1:end/2);
-
-  r = V(1:n, 1:end/2);
-  l = V((n + 1):2 * n, 1:end/2);
+  
+  LAMBDA = diag(LAMBDA);
+  
+  r = V(1:n, :);
+  l = V((n + 1):2 * n, :);
 
   s = zeros(1, columns(r));
 
@@ -44,6 +51,27 @@ function [r, l, lambda, err, LAMBDA] = mbdyn_post_modal_decomposition(A, B, dCoe
   r *= s;
   l *= s;
 
+  ortho = true(1, columns(r));
+
+  if (~isfield(opts, "tol_ortho"))
+    opts.tol_ortho = eps^0.5;
+  endif
+  
+  for i=1:columns(ortho)
+    for j=i + 1:columns(ortho)
+      if (ortho(j))
+        ortho(j) = abs(l(:, i).' * B * r(:, j)) < opts.tol_ortho;
+      endif
+    endfor
+  endfor
+
+  idx = find(ortho);
+
+  LAMBDA = LAMBDA(idx);
+  
+  r = r(:, idx);
+  l = l(:, idx);
+  
   err = 0;
 
   for i=1:columns(r)
@@ -55,6 +83,15 @@ function [r, l, lambda, err, LAMBDA] = mbdyn_post_modal_decomposition(A, B, dCoe
   endfor
 
   lambda = (LAMBDA - 1) ./ (LAMBDA + 1) / dCoef;
+
+  O = [abs(imag(lambda)), imag(lambda), real(lambda)];
+
+  [~, idx] = sortrows(O, [1, -2, 3]);
+
+  lambda = lambda(idx);
+  LAMBDA = LAMBDA(idx);
+  l = l(:, idx);
+  r = r(:, idx);
 endfunction
 
 function Y = mbdyn_modal_operator_func(A, B, n, X)
@@ -72,34 +109,67 @@ endfunction
 %!   [R,lambda] = eig(C);
 %!   lambda = diag(lambda);
 %!   A = [abs(imag(lambda(:))), imag(lambda(:))];
-%!   [~, idx] = sortrows(A, [-1,-2]);
+%!   [~, idx] = sortrows(A, [1, -2]);
 %!   lambda = lambda(idx);
 %!   R = R(:, idx);
 %!   if ( nargout() >= 3 )
 %!     f = imag(lambda(2*(1:length(lambda)/2)-1)) / ( 2 * pi );
 %!   endif
 %! endfunction
-%! m1 = 10;
-%! m2 = 100;
-%! k1 = 10000;
-%! k2 = 100000;
-%! M = [1 / 3 * m1,        1 / 6 * m1,       0;
-%!      1 / 6 * m1, 1 / 3 * (m1 + m2), 1 / 6 * m2;
-%!               0,        1 / 6 * m2, 1 / 3 * m2];
+%! n = 8;
+%! elno = [1, 2;
+%!         2, 3;
+%!         3, 4,
+%!         4, 5;
+%!         5, 6;
+%!         6, 7;
+%!         7, 8;
+%!         8, 0];
+%! elm = [1000;
+%!        1000;
+%!        1000;
+%!        1000;
+%!        1000;
+%!        1000;
+%!        1000;
+%!        1000];
+%! elk = [10000;
+%!        10000;
+%!        10000;
+%!        10000;
+%!        10000;
+%!        10000;
+%!        10000;
+%!        10000];
+%! M = zeros(n, n);
+%! K = zeros(n, n);
+%! for i=1:rows(elno)
+%!   Mi = [1 / 3 * elm(i), 1 / 6 * elm(i);
+%!         1 / 6 * elm(i), 1 / 3 * elm(i)];
+%!   Ki = [elk(i), -elk(i);
+%!         -elk(i), elk(i)];
+%!   for j=1:2
+%!     for k=1:2
+%!       if (elno(i, j) > 0 && elno(i, k) > 0)
+%!       M(elno(i, j), elno(i, k)) += Mi(j, k);
+%!       K(elno(i, j), elno(i, k)) += Ki(j, k);
+%!       endif
+%!     endfor
+%!   endfor
+%! endfor
+%! assert(isdefinite(M));
+%! assert(isdefinite(K));
 %!
-%! K = [ k1,      -k1,        0;
-%!      -k1,  k1 + k2,      -k2;
-%!        0,      -k2,  k1 + k2];
-%!
-%! beta = 0.2;
+%! beta = 0.05;
 %!
 %! D = beta * K;
 %! f_constraint = true;
 %! if (f_constraint)
-%!   Phi = [-1, 0, 1];
+%!   Phi = zeros(1, n);
+%!   Phi(1:2) = [1, -1];
 %!   T = null(Phi);
 %! else
-%!   Phi = zeros(0, 3);
+%!   Phi = zeros(0, n);
 %!   T = eye(3);
 %! endif
 %! Mred = T.' * M * T;
@@ -107,8 +177,8 @@ endfunction
 %! Dred = T.' * D * T;
 %! [PHI_ref, lambda_ref, f_ref] = damped_eigenmode(Mred, Dred, Kred);
 
-%! fmin = 0.1 * max(f_ref);
-%! if (~fmin > 0)
+%! fmin = 0.1 * min(f_ref);
+%! if (~(fmin > 0))
 %!   error("estimation for dCoef failed");
 %! endif
 %! dCoef = 5 / (2 * pi * fmin);
@@ -127,9 +197,10 @@ endfunction
 %! A = sparse(A);
 %! B = sparse(B);
 %! sigma = "li";
-%! k = 4;
+%! k = 14;
+%! opts.p = 2 * columns(A);
 %! opts.solver = "umfpack";
-%! opts.refine_max_iter = 10;
+%! opts.refine_max_iter = 20;
 %! opts.pre_scaling = true;
 %! opts.maxit = 5000;
 %! opts.tol = 0;
@@ -139,5 +210,5 @@ endfunction
 %! assert(err < tol);
 %! f = imag(lambda) / (2 * pi);
 %! assert(VL.' * A * VR, diag(LAMBDA), tol*norm(LAMBDA));
-%! assert(VL.' * B * VR, eye(2), tol);
-%! assert(lambda, lambda_ref(1:2), tol*norm(lambda_ref(1:2)));
+%! assert(VL.' * B * VR, eye(columns(VL)), tol);
+%! assert(lambda, lambda_ref(1:rows(lambda)), tol*norm(lambda_ref(1:rows(lambda))));
